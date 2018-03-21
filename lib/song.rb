a0b9069d6f68318ca45f95d5487afcda8a2fea63
David Markel <davidgmarkel@gmail.com>
2018-03-21 18:56:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,9 +8,7 @@ class Song
   @@artist_count = {}
 
   def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
+    
     @@count += 1
     @@artists << artist
     @@genres << genre
